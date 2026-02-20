ac644d04a07f4d16bb1bce08e3d1f96145dce95a
Louis Beal <louis.j.beal@gmail.com>
2026-02-20 18:45:28 +0100

    Remove legacy test

--- tests/test_init.py
+++ tests/test_init.py
@@ -14,15 +14,6 @@ def test_no_args() -> None:
         ConfigTest()
 
 
-def test_both_args() -> None:
-    """Tests that providing both path and paths prioritises path."""
-
-    config = ConfigTest(path="config1.yaml", paths=["config2.yaml", "config3.yaml"])
-    assert config._paths == ["config1.yaml"]
-    assert config.path == Path("config1.yaml")
-    assert config.filename == "config1.yaml"
-
-
 def test_path_as_list() -> None:
     """Tests that providing path as a list works."""
 

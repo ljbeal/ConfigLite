7bad4ad81269418bff6c058cdd520d4f42517ded
Louis Beal <louis.j.beal@gmail.com>
2026-02-24 11:50:05 +0100

    Add tests to ensure that configs are properly adopted

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pytest
+import yaml
 from configlite.config import BaseConfig
+from tests.conftest import verify_variable
 
 
 class ConfigTest(BaseConfig):
@@ -43,3 +45,16 @@ def test_empty_paths():
     """Tests that providing empty paths raises an error."""
     with pytest.raises(ValueError):
         ConfigTest(paths=[])
+
+
+def test_file_adoption() -> None:
+    """Ensures that a pre-existing file is not overwritten by a new config creation."""
+    file = Path("precreate.yaml")
+    with file.open("w+") as o:
+        yaml.safe_dump({"foo": "bar"}, o)
+
+    cfg = ConfigTest(file)
+
+    assert cfg.foo == "bar"
+    assert cfg["foo"] == "bar"
+    assert verify_variable(file, "foo", "bar")

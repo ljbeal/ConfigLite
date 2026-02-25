a5c7a89ed6471d4d46dddfad4137aa8ff76699e2
Louis Beal <louis.j.beal@gmail.com>
2026-02-25 08:28:20 +0100

    Drop deferredvalue

--- configlite/config.py
+++ configlite/config.py
@@ -77,33 +77,3 @@ class BaseConfig(FileMixin):
     def attributes(self) -> list[str]:
         """List of attributes that are defined in this config."""
         return list(self.data)
-
-
-class DeferredValue:
-    """Stub class for deferring value access to a file read.
-
-    Interacts with `__getattribute__` to "defer" reads to file.
-    In Config, we first use the base object.__getattribute__(...) to collect an arbitrary item
-    If that item has been replaced by a `DeferredValue` object, then we read from file
-    Otherwise, return  the item
-
-    Args:
-        value (str): The name of the variable to access.
-    """
-
-    __slots__ = ["_parent", "_value"]
-
-    def __init__(self, value: str) -> None:
-        """Create the stub.
-
-        Args:
-            value: The name of the variable to access.
-        """
-        if not isinstance(value, str):
-            raise TypeError("Value target must be a string")
-
-        self._value = value
-
-    @property
-    def value(self) -> str:
-        return self._value

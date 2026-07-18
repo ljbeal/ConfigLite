6079cbab8c834dc0f445392c2ed67a293c269930
ljbeal <39353757+ljbeal@users.noreply.github.com>
2026-07-18 19:12:14 +0200

    34 environment variables are cached at init (#35)
    * Always parse environment on get
    
    * Fix an issue that could cause a falsy default to be skipped

--- tests/test_from_prefix.py
+++ tests/test_from_prefix.py
@@ -10,30 +10,50 @@ class ConfigTest(BaseConfig):
     prefix = "MYPREFIX"
 
 
-def test_override_with_env(monkeypatch: pytest.MonkeyPatch) -> None:
-    """Test that we return the value of a set environment variable."""
-    monkeypatch.setenv("MYPREFIX_INIT_VAR", "hello")
-    cfg = ConfigTest(path="test_config.yaml", autocreate=True)
-    assert cfg.get("INIT_VAR") == "hello"
+class ConfigTestUnderscore(BaseConfig):
+    defaults = {
+        "INIT_VAR": "foo",
+    }
+    prefix = "MYPREFIX_"
 
 
-def test_missing_no_default(monkeypatch: pytest.MonkeyPatch) -> None:
-    """Check that we're not just picking up any old variables."""
-    monkeypatch.delenv("MYPREFIX_NONE_VAR", raising=False)
-    cfg = ConfigTest(path="test_config.yaml", autocreate=True)
-    with pytest.raises(KeyError, match=".*not found in Config!"):
-        cfg.get("NONE_VAR") is None
+@pytest.mark.parametrize("cfg", [ConfigTest, ConfigTestUnderscore])
+class TestEnvOverride:
+    def test_override_with_env(self, cfg, monkeypatch: pytest.MonkeyPatch) -> None:
+        """Test that we return the value of a set environment variable."""
+        monkeypatch.setenv("MYPREFIX_INIT_VAR", "hello")
+        cfg = cfg(path="test_config.yaml", autocreate=True)
+        assert cfg.get("INIT_VAR") == "hello"
+        assert cfg.INIT_VAR == "hello"
+        assert cfg["INIT_VAR"] == "hello"
 
+    def test_missing_no_default(self, cfg, monkeypatch: pytest.MonkeyPatch) -> None:
+        """Check that we're not just picking up any old variables."""
+        monkeypatch.delenv("MYPREFIX_NONE_VAR", raising=False)
+        cfg = cfg(path="test_config.yaml", autocreate=True)
+        with pytest.raises(KeyError, match=".*not found in Config!"):
+            cfg.get("NONE_VAR") is None
 
-def test_missing_with_default(monkeypatch: pytest.MonkeyPatch) -> None:
-    """Check that we can still default."""
-    monkeypatch.delenv("MYPREFIX_NONE_VAR", raising=False)
-    cfg = ConfigTest(path="test_config.yaml", autocreate=True)
-    assert cfg.get("NONE_VAR", "fallback") == "fallback"
+    def test_missing_with_default(self, cfg, monkeypatch: pytest.MonkeyPatch) -> None:
+        """Check that we can still default."""
+        monkeypatch.delenv("MYPREFIX_NONE_VAR", raising=False)
+        cfg = cfg(path="test_config.yaml", autocreate=True)
+        assert cfg.get("NONE_VAR", "fallback") == "fallback"
 
+    def test_missing_with_default_on_class(
+        self, cfg, monkeypatch: pytest.MonkeyPatch
+    ) -> None:
+        """Returns the default value when the variable is not set."""
+        monkeypatch.delenv("MYPREFIX_INIT_VAR", raising=False)
+        cfg = cfg(path="test_config.yaml", autocreate=True)
+        assert cfg.get("INIT_VAR") == "foo"
+        assert cfg.INIT_VAR == "foo"
+        assert cfg["INIT_VAR"] == "foo"
 
-def test_missing_with_default_on_class(monkeypatch: pytest.MonkeyPatch) -> None:
-    """Returns the default value when the variable is not set."""
-    monkeypatch.delenv("MYPREFIX_INIT_VAR", raising=False)
-    cfg = ConfigTest(path="test_config.yaml", autocreate=True)
-    assert cfg.get("INIT_VAR") == "foo"
+    def test_get_reads_current_env(self, cfg, monkeypatch: pytest.MonkeyPatch) -> None:
+        """Test that `get` reflects the true state of the environment."""
+        cfg = cfg(path="test_config.yaml", autocreate=True)
+        monkeypatch.setenv("MYPREFIX_INIT_VAR", "hello")
+        assert cfg.get("INIT_VAR") == "hello"
+        assert cfg.INIT_VAR == "hello"
+        assert cfg["INIT_VAR"] == "hello"

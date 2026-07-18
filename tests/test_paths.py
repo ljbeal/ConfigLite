01ebb3cd682ff7a774966698d679d410bdb53eb1
Louis Beal <louis.j.beal@gmail.com>
2026-07-18 18:25:48 +0200

    Fix path tests

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 
 import yaml
+
 from configlite.config import BaseConfig
+from tests.conftest import verify_variable
 
 
 class ConfigTest(BaseConfig):
@@ -11,43 +13,61 @@ class ConfigTest(BaseConfig):
     }
 
 
-def test_default_to_last():
+def test_default_to_last(tmpdir):
     """Tests that the default path is the last one in the list."""
 
-    workdir = Path(os.getcwd()) / "config_local.yaml"
-    lastdir = workdir / ".config" / "config.yaml"
+    firstpath = tmpdir / "config_local.yaml"
+    lastpath = tmpdir / ".config" / "config.yaml"
 
-    config = ConfigTest(paths=[workdir, lastdir])
+    config = ConfigTest(paths=[firstpath, lastpath])
 
-    assert config.path == lastdir
+    assert config.path == lastpath
 
     # now test that creating a config in the higher priority directories works
-    with open(workdir, "w+") as o:
+    with open(firstpath, "w+") as o:
         yaml.dump({"foo": "bar"}, o)
 
-    assert config.path == workdir
+    assert config.path == firstpath
     assert config.foo == "bar"
 
 
-def test_inner_dir_access():
+def test_inner_dir_access(tmpdir):
     """Tests that configs in inner directories can be accessed."""
-    cfg = ConfigTest(path="inner/config.yaml")
+    inner_dir = tmpdir / "inner"
+    cfg = ConfigTest(path=inner_dir / "config.yaml")
 
-    assert not Path("inner").exists()
+    assert inner_dir.exists()
 
     assert cfg.foo == "foo"
+    verify_variable(cfg.path, "foo", "foo")
 
 
-def test_stacked_files() -> None:
+def test_stacked_files(tmpdir) -> None:
     """Test that specifying both path and paths does not cause problems."""
-    cfg = ConfigTest(path="config.yaml", paths=["backup.yaml", "another.yaml"])
+    cfg = ConfigTest(
+        path=tmpdir / "config.yaml",
+        paths=[tmpdir / "backup.yaml", tmpdir / "another.yaml"],
+        autocreate=False,
+    )
 
     assert len(cfg._paths) == 3
-    assert cfg.paths[0] == Path("config.yaml")
+    assert cfg.paths[0] == tmpdir / "config.yaml"
 
     # check path list
-    assert cfg.paths == [Path(p) for p in ["config.yaml", "backup.yaml", "another.yaml"]]
-    # make sure that the paths list is exactly as specified
-    assert cfg.paths != [Path(p).resolve() for p in ["config.yaml", "backup.yaml", "another.yaml"]]
+    assert cfg.paths == [
+        Path(p)
+        for p in [
+            tmpdir / "config.yaml",
+            tmpdir / "backup.yaml",
+            tmpdir / "another.yaml",
+        ]
+    ]
     # also add a check to ensure we _can_ get abspaths
-    assert cfg.abspaths == [Path(p).resolve() for p in ["config.yaml", "backup.yaml", "another.yaml"]]
+    assert cfg.abspaths == [
+        Path(p).resolve()
+        for p in [
+            tmpdir / "config.yaml",
+            tmpdir / "backup.yaml",
+            tmpdir / "another.yaml",
+        ]
+    ]
